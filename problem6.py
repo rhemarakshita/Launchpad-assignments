027d9b3a80727cff5d4cec887e561406e17c3c2b
rhemarakshita <61504015+rhemarakshita@users.noreply.github.com>
2020-02-27 00:55:57 +0530

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,11 @@
+D={}
+value1 = input("Enter name:")
+key1   = input("Enter USN:")
+value2 = input("Enter name:")
+key2   = input("Enter USN:")
+value3 = input("Enter name:")
+key3   = input("Enter USN:")
+D[key1]= value1
+D[key2]= value2
+D[key3]= value3
+print(D)
